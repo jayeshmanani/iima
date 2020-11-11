28e87927802ea313bbcd039c8d6b0fd514890c95
Jayesh Manani <manani.jayesh5@gmail.com>
2020-11-11 10:51:57 +0530

    Update the admin.py with example of admin
    https://medium.com/analytics-vidhya/create-a-fullstack-app-with-django-graphql-and-vuejs-727a0cf41820

--- data_website/firstUI/admin.py
+++ data_website/firstUI/admin.py
@@ -5,3 +5,19 @@ from firstUI.models import FinanceData
 
 admin.site.register(Country)
 admin.site.register(FinanceData)
+
+
+
+# from this website -> https://medium.com/analytics-vidhya/create-a-fullstack-app-with-django-graphql-and-vuejs-727a0cf41820
+
+# from django.contrib import admin
+# from .models import *
+# class CityAdmin(admin.ModelAdmin):
+#     fields = (‘city_name’,)
+# class TitleAdmin(admin.ModelAdmin):
+#     fields = (‘title_name’,)
+# class EmployeeAdmin(admin.ModelAdmin):
+#     fields = (‘employee_name’, ‘employee_city’, ‘employee_title’,)
+# admin.site.register(City, CityAdmin)
+# admin.site.register(Title, TitleAdmin)
+# admin.site.register(Employee, EmployeeAdmin)
