77c19587612832edeb349b7632bc93ac2778683f
Jayesh Manani <manani.jayesh5@gmail.com>
2020-11-12 23:14:38 +0530

    updated the admin file with location of model

--- data_website/firstUI/admin.py
+++ data_website/firstUI/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from firstUI.models import Country
-from firstUI.models import FinanceData
+from .models import Country # from firstUI.models import Country
+from .models import FinanceData # from firstUI.models import FinanceData
+
 # Register your models here.
 
 admin.site.register(Country)
