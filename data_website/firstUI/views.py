92415f46ea6ebe7faeb667c9af66af8bbc4fc0ab
Jayesh Manani <manani.jayesh5@gmail.com>
2020-11-20 19:14:01 +0530

    updated views and logic for piechart

--- data_website/firstUI/views.py
+++ data_website/firstUI/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .functions import check_if_data, refresh_data #from firstUI.functions import check_if_data, refresh_data
 import datetime
 import pandas as pd
+import numpy as np
 from .models import FinanceData # from firstUI.models import FinanceData
 from .models import Country # from firstUI.models import Country
 
@@ -157,22 +158,78 @@ def updateStatus(request):
 
 def pieChart(request):
     is_pie_Debt = 1
-    debt_labels = ['L.totexp~p','L.realgd~c','L.totres~p','L.fiscal~o','L.pv_est','dum_def_~5','L.ge_est','L.realgd~r','L.cpich~o'] #8 variable
-    debt_data = [0.1355, 0.1287, 0.1105, 0.0686, 0.0406, 0.0809, 0.3447, 0.0200, 0.0705]
+    country_l = list(Country.objects.all().values())
+    debt_labels = ['External Debt/Exports', 'Realgdp_pc', 'Reserves/imports', 'Fiscalbalance/gdp', 'Political stability', 'Default history', 'Government effectiveness', 'Real GDP growth', 'Inflation'] #9 variable
+    debt_data = [0.1601, 0.1534, 0.1141, 0.0681, 0.0374, 0.0479, 0.3417, 0.0143, 0.0631]
     labels = debt_labels
     data = debt_data
     fontColour = ["black"]
     fontColour = fontColour*10
     if request.method == 'POST':
+        yr = int(request.POST['year'])
+        # yr -= 1
+        cntr = request.POST['country']
+        cnn = Country.objects.filter(country_name = cntr)
+        for i in cnn:    
+            cnn_id = i.id
+        # dtt = FinanceData.objects.filter(data_year=yr, country_id=cnn_id).get()
+        dtt = check_if_data(yr)
+        dtt = dtt[dtt.id == cnn_id]
+        # print(dtt.country_name.values[0])
+        # print(type(dtt.Speculative_Grade_Prob) == type(None))
+        # print(dtt.Debt_Distress_prob)
         vals = request.POST['piePlts']
         if vals == 'specGradeProb':
             is_pie_Debt = 0
-            spec_labels = ['L.logrea~c','L.saving~o','L.totres~b','L.rl_est','L.dum_de~s','L.govtgr~o','L.m3gdp_~d','L.export~o','L.fiscal~o'] #9 variable
-            spec_data = [0.2483, 0.1514, 0.0304, 0.2925, 0.0406, 0.1199, 0.0611, 0.0025, 0.0534]
-            labels = spec_labels
-            data = spec_data
+            spec_labels = ['LogrealGNI_pc', 'Savings/gdp', 'Rule of law', 'Govt gross debt'] #4 variable
+            spec_data = [0.2483, 0.1514, 0.2925, 0.1199]
+            
+            # first model in spec grade
+            # spec_val = dtt.Speculative_Grade_Prob.values[0]
+            # if type(spec_val) != type(None) and str(spec_val) != 'nan':
+            #     # print(spec_val)
+            #     spec_data = list(np.array(spec_data) * spec_val)
+            # data = spec_data # first model - multiple constant value by spec data value
 
-    context = {'is_pie_Debt':is_pie_Debt, 'labels':labels, 'data':data, 'fontColour':fontColour}
+            # second model in spec grade
+            data2 = []
+            spec_fetched = [dtt.log_real_gnipc, dtt.savings_gdp, dtt.rule_of_law, dtt.government_grossdebt_gdp]
+            for enm, spc in enumerate(spec_fetched):
+                if (type(spc.values[0]) != type(None)) and (str(spc.values[0]) != 'nan'):
+                    data2.append(spec_data[enm] * spc.values[0])
+                else:
+                    data2.append(spec_data[enm])
+            data = data2 #second model - individual values multiply
+            
+            labels = spec_labels
+
+            # print("This is spec data : ", data)
+
+        elif vals == 'debtDistressProb':
+            # first model in debt distress
+            # if type(dtt.Debt_Distress_prob.values[0]) != type(None):
+            #     # print(debt_data)
+            #     debt_data = list(np.array(debt_data) * dtt.Debt_Distress_prob.values[0])
+            # data = debt_data
+
+            # second model in debt distress
+            data2 = []
+            debt_fetched = [dtt.externaldebt_exports, dtt.real_gdppc, dtt.reserves_import, dtt.fiscal_balance_gdp, dtt.political_stability, dtt.default_hist, dtt.govt_effectiveness, dtt.real_gdpgrowth, dtt.inflation_data]
+            for enm, debt in enumerate(debt_fetched):
+                if enm == 5:
+                    if debt.values[0]:
+                        data2.append(debt_data[enm] * 1)
+                    else:
+                        data2.append(debt_data[enm] * 0)
+                elif (type(debt.values[0]) != type(None)):
+                    data2.append(debt_data[enm] * debt.values[0])
+                else:
+                    data2.append(debt_data[enm])
+            data = data2 #second model - individual values multiply
+
+            # print("This is debt data : ",data)
+
+    context = {'is_pie_Debt':is_pie_Debt, 'labels':labels, 'data':data, 'fontColour':fontColour, 'country_l':country_l}
     return render(request, 'pieChart.html', context=context)
 
 def refreshData(request):
